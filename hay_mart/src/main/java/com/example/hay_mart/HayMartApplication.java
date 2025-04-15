234aa45d407b39c7021a7347ca5b2676ae570dbc
Hilwa Isnaini Marfuah <isnainihilwa19@gmail.com>
2025-04-15 08:12:42 +0700

    mbenerin lapen

--- hay_mart/src/main/java/com/example/hay_mart/HayMartApplication.java
+++ hay_mart/src/main/java/com/example/hay_mart/HayMartApplication.java
@@ -2,12 +2,12 @@ package com.example.hay_mart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HayMartApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HayMartApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HayMartApplication.class, args);
+    }
 }
---
+++ hay_mart/src/test/java/com/example/hay_mart/HayMartApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.hay_mart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HayMartApplicationTest {
+	@Test
+	void contextLoads() {
+	}
+
+}
